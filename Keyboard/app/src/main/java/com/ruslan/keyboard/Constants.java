56631e142b74737ad213838775002f6bc2682c4d
Ruslan-Far <rfarhetdinov@mail.ru>
2023-04-12 14:50:01 +0300

    выравнял текст в TextView и поменял цвет темы

--- Keyboard/app/src/main/java/com/ruslan/keyboard/Constants.java
+++ Keyboard/app/src/main/java/com/ruslan/keyboard/Constants.java
@@ -2,7 +2,9 @@ package com.ruslan.keyboard;
 
 public class Constants {
 
-    public static final String API_BASE_URL = "http://192.168.1.100:8080/";
+//    public static final String API_BASE_URL = "http://192.168.1.100:8080/";
+    public static final String API_BASE_URL = "http://192.168.69.71:8080/";
+
 //    public static final String API_BASE_URL = "http://192.168.87.71:8080/";
 //    public static final String API_BASE_URL = "http://192.168.0.60:8080/";
 //    public static final String API_BASE_URL = "http://192.168.1.102:8080/";
